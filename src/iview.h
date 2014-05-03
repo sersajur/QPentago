e5958e5f8552a55dcb74e042c890f3aeacfac6ed
sersajur <sersajur@gmail.com>
2014-05-03 22:33:29 +0300

    add GUI_interface

--- src/iview.h
+++ src/iview.h
@@ -2,28 +2,31 @@
 #define IVIEW_H
 
 #include <string>
-#include <QColor>
+
 using std::string;
+
 class IView
 {
 public:
     enum control_setting
     {
-        MENU,          /*are enable: New_game(),Load_game(),Join_game()*/
+        MENU,          /*are enable: New_game(),Load_game(),Join_game(),Host_game()*/
         LOCAL_GAME,    /*are enable: Save_game(),Put_stone(),Rotate(),Leave()*/
         NETWORK_GAME   /*are enable: Put_stone(),Rotate(),Leave()*/
     };
-    enum quadrant{I=1,II,III,IV};/* for    */
-    enum turn{left, right};      /* Rotate() */
+    enum quadrant{I=1,II,III,IV};/* for         */
+    enum turn{LEFT, RIGHT};      /*  Rotate()   */
+    enum color{BLACK, WHITE};    /* for Draw_stone() */
 public:
     virtual void Set_control_settings(control_setting param)=0;/*enable/disable graphic control elements*/
-    virtual void Draw_stone(int row, int col, int color)=0;/*draw stone at [row, col] with color*/
+    virtual void Draw_stone(int row, int col, color c)=0;/*draw stone at [row, col] with color*/
     virtual void Show_message(string text)=0;/*print some log text*/
 public:
     virtual void New_game(int player_count)=0;
     virtual void Save_game(string file_name)=0;
     virtual void Load_game(string file_name)=0;
-    virtual void Join_game(string connect_data)=0;
+    virtual void Join_game(string join_connect_data)=0;
+    virtual void Host_game(string host_connect_data)=0;
     virtual void Put_stone(int row, int col)=0; /*enable Rotate()*/
     virtual void Rotate(quadrant num, turn t)=0;/*disable Rotate() */
     virtual void Leave()=0;
