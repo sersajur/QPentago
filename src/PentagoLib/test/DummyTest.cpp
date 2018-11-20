3295784f319ed85647c37ed661fea272cd13f4e0
sersajur <sersajur@gmail.com>
2018-11-20 20:13:07 +0200

    Removed failing dummy tests

--- src/PentagoLib/test/DummyTest.cpp
+++ src/PentagoLib/test/DummyTest.cpp
@@ -6,17 +6,7 @@ TEST(Dummy, ExpectPass)
   EXPECT_FALSE(false);
   }
 
-TEST(Dummy, ExpectFail)
-  {
-  EXPECT_FALSE(true);
-  }
-
 TEST(Dummy, AssertPass)
   {
   ASSERT_FALSE(false);
-  }
-
-TEST(Dummy, AssertFail)
-  {
-  ASSERT_FALSE(true);
   }
